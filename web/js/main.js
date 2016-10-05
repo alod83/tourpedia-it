6347e6de515beadfbd56c19c509c1e0d59202f44
GiacoGreg <giacomo.gregori87@gmail.com>
2016-10-05 11:44:12 +0200

    Responsività e aggiustamenti
    - Migliorata la responsività del sito attraverso il JavaScript
    - Corrette le dimensioni delle sezioni per evitare tagli errati a
    seconda delle dimensioni della finestra

--- web/js/main.js
+++ web/js/main.js
@@ -2,7 +2,8 @@
 $(document).ready(function() {
 	Mappa($('input[name="type"]:checked').val());
 	Grafico();
-	
+	dimensionitabelle();
+	//alert($(window).height());
 	
 	// Radio Record
 	$("#record").bind('click', function() {
@@ -18,4 +19,58 @@ $(document).ready(function() {
 	$("#territorio").bind('click', function() {
 		Mappa("recterr");
 	});
+	
+// Ridimensionamento pagina
+	$(window).resize(function(){
+		dimensionitabelle();
+		//alert($(window).width());
+	});
 });
+
+// Funzione che ridimensiona le tabelle della sezione Fonti
+function dimensionitabelle() {
+	if ($(window).width()>768){
+			$(".tab_fonti_sx").css("width",($(".col-lg-12").width())*30/100);
+			$(".tab_fonti_cx").css("width",($(".col-lg-12").width())*30/100);
+			$(".tab_fonti_dx").css("width",($(".col-lg-12").width())*30/100);
+			$(".tab_fonti_sx").css("margin-left",($(".col-lg-12").width())*3/100);
+			$(".tab_fonti_sx").css("margin-right",($(".col-lg-12").width())*3/100);
+			$(".tab_fonti_cx").css("margin-left","0");
+			$(".tab_fonti_cx").css("margin-right","0");
+			$(".tab_fonti_dx").css("margin-left",($(".col-lg-12").width())*3/100);
+			$(".tab_fonti_dx").css("margin-right",($(".col-lg-12").width())*3/100);
+			if ($(window).width()<975){
+				$(".tab_fonti_dx").css("margin-top","6px");
+			}
+			if ($(window).width()>974){
+				$(".tab_fonti_dx").css("margin-top","12px");
+			}
+		}
+	if ($(window).width()<769){
+		$(".tab_fonti_sx").css("width",($(".col-lg-12").width())*50/100);
+		$(".tab_fonti_cx").css("width",($(".col-lg-12").width())*50/100);
+		$(".tab_fonti_dx").css("width",($(".col-lg-12").width())*50/100);
+		$(".tab_fonti_sx").css("margin-left",($(".col-lg-12").width())*25/100);
+		$(".tab_fonti_sx").css("margin-right",($(".col-lg-12").width())*25/100);
+		$(".tab_fonti_cx").css("margin-left",($(".col-lg-12").width())*25/100);
+		$(".tab_fonti_cx").css("margin-right",($(".col-lg-12").width())*25/100);
+		$(".tab_fonti_dx").css("margin-left",($(".col-lg-12").width())*25/100);
+		$(".tab_fonti_dx").css("margin-right",($(".col-lg-12").width())*25/100);
+		$(".tab_fonti_dx").css("margin-top","10px");
+		if ($(window).width()>500){
+			$(".tab_fonti_dx").css("margin-top",($(".tab_fonti_sx").height())+45);
+		}
+		if ($(window).width()<501){
+				$(".tab_fonti_sx").css("width","207");
+				$(".tab_fonti_sx").css("margin-left",(($(".col-lg-12").width())-207)/2);
+				$(".tab_fonti_sx").css("margin-right",(($(".col-lg-12").width())-207)/2);
+				$(".tab_fonti_cx").css("width","207");
+				$(".tab_fonti_cx").css("margin-left",(($(".col-lg-12").width())-207)/2);
+				$(".tab_fonti_cx").css("margin-right",(($(".col-lg-12").width())-207)/2);
+				$(".tab_fonti_dx").css("margin-top","10");
+				$(".tab_fonti_dx").css("width","207");
+				$(".tab_fonti_dx").css("margin-left",(($(".col-lg-12").width())-207)/2);
+				$(".tab_fonti_dx").css("margin-right",(($(".col-lg-12").width())-207)/2);
+			}
+	}
+}
