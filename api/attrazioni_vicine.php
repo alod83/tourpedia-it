37c3c0395af6b3322d049e81a6e9c91f0cdbac01
SimoBaldo94 <32310285+SimoBaldo94@users.noreply.github.com>
2018-05-07 16:45:26 +0200

    correzione api attrazioni_vicine.php

--- api/attrazioni_vicine.php
+++ api/attrazioni_vicine.php
@@ -18,10 +18,12 @@ $connection->selectDB('db_attraction');
 //$collection_name = $ini_array['Mongo']['collection'];
 //$collection = $dbname->$ini_array['Mongo']['collection'];
 //$collection = $connection->$db_name->$collection_name;
-$query = array('$and' => array( array('latitude' => array( '$gt' => $latitudine-0.001, '$lt' => $latitudine+0.001 )), array('longitude' => array( '$gt' => $longitudine-0.001, '$lt' => $longitudine+0.001 )) ));
+//$query = array('$and' => array( array('latitude' => array( '$gt' => $latitudine-0.001, '$lt' => $latitudine+0.001 )), array('longitude' => array( '$gt' => $longitudine-0.001, '$lt' => $longitudine+0.001 )) ));
+$query = array( 'latitude' => array( '$gt' => $latitudine-0.001, '$lt' => $latitudine+0.001 ), 'longitude' => array( '$gt' => $longitudine-0.001, '$lt' => $longitudine+0.001 ));
 $result=iterator_to_array($connection->find($query));
 if(count($result) == 0){
-	$query = array('$and' => array( array('latitude' => array( '$gt' => $latitudine-0.01, '$lt' => $latitudine+0.01 )), array('longitude' => array( '$gt' => $longitudine-0.01, '$lt' => $longitudine+0.01 )) ));
+	//$query = array('$and' => array( array('latitude' => array( '$gt' => $latitudine-0.01, '$lt' => $latitudine+0.01 )), array('longitude' => array( '$gt' => $longitudine-0.01, '$lt' => $longitudine+0.01 )) ));
+	$query = array( 'latitude' => array( '$gt' => $latitudine-0.01, '$lt' => $latitudine+0.01 ), 'longitude' => array( '$gt' => $longitudine-0.01, '$lt' => $longitudine+0.01 ));
 	$result=iterator_to_array($connection->find($query));
 }
 if(count($result) > 10){
