006d347f10501026e8d694a558164ec5df51c449
Stefano Abbate <stefano.abbate@iit.cnr.it>
2018-05-09 17:04:34 +0200

    aggiornato codice attrazioni vicine

--- api/attrazioni_vicine.php
+++ api/attrazioni_vicine.php
@@ -6,7 +6,7 @@ $longitudine = $_REQUEST['lon'];
 $array=array();
 
 //require('../vendor/autoload.php');
-include('../libraries/mongodb.php');
+include('../libraries/mongodb_old.php');
 $ini_array = parse_ini_file("../update/config.ini", true);
 $mongo_url = $ini_array['Mongo']['url'];
 //$connection = new MongoClient($mongo_url);
@@ -20,19 +20,27 @@ $connection->selectDB('db_attraction');
 //$collection = $connection->$db_name->$collection_name;
 //$query = array('$and' => array( array('latitude' => array( '$gt' => $latitudine-0.001, '$lt' => $latitudine+0.001 )), array('longitude' => array( '$gt' => $longitudine-0.001, '$lt' => $longitudine+0.001 )) ));
 $query = array( 'latitude' => array( '$gt' => $latitudine-0.001, '$lt' => $latitudine+0.001 ), 'longitude' => array( '$gt' => $longitudine-0.001, '$lt' => $longitudine+0.001 ));
+
 $result=iterator_to_array($connection->find($query));
+
 if(count($result) == 0){
 	//$query = array('$and' => array( array('latitude' => array( '$gt' => $latitudine-0.01, '$lt' => $latitudine+0.01 )), array('longitude' => array( '$gt' => $longitudine-0.01, '$lt' => $longitudine+0.01 )) ));
 	$query = array( 'latitude' => array( '$gt' => $latitudine-0.01, '$lt' => $latitudine+0.01 ), 'longitude' => array( '$gt' => $longitudine-0.01, '$lt' => $longitudine+0.01 ));
 	$result=iterator_to_array($connection->find($query));
 }
 if(count($result) > 10){
-	for($i=0; $i<10; $i++){
-		array_push($array , $result[$i]);
+	$i = 0;
+	foreach ($result as $record){
+		if($i < 10)
+		{
+			array_push($array , $record);
+			$i++;
+		}else
+			break;
 	}
 }else{
-	for($i=0; $i<count($result); $i++){
-		array_push($array , $result[$i]);
+	foreach ($result as $record){
+		array_push($array , $record);
 	}
 }
 echo(json_encode($array));
