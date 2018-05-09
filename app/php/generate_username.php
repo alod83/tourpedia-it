6b1c71747122c4442a92499a3bbb5c8ae138226e
SimoBaldo94 <32310285+SimoBaldo94@users.noreply.github.com>
2018-05-09 17:12:30 +0200

    correzione api per la generazione degli account

--- app/php/generate_username.php
+++ app/php/generate_username.php
@@ -11,22 +11,25 @@ function randomPassword() {
     return implode($pass); //turn the array into a string
 }
 
-require('../../vendor/autoload.php');
+//require('../../vendor/autoload.php');
+include('../../libraries/mongodb.php');
 
 $ini_array = parse_ini_file("../../update/config.ini", true);
 
 $mongo_url = $ini_array['Mongo']['url'];
 
 //$connection = new MongoClient($mongo_url);
-$connection = new MongoDB\Client($mongo_url);
+//$connection = new MongoDB\Client($mongo_url);
+$connection = new MyMongoClient($mongo_url, $ini_array);
 
 //$dbname = $connection->selectDB($ini_array['Mongo']['db_accommodation']);
-$db_name = $ini_array['Mongo']['db_accommodation'];
-$collection_name = $ini_array['Mongo']['collection'];
+//$db_name = $ini_array['Mongo']['db_accommodation'];
+//$collection_name = $ini_array['Mongo']['collection'];
 //$collection = $dbname->$ini_array['Mongo']['collection'];
-$collection = $connection->$db_name->$collection_name;
+//$collection = $connection->$db_name->$collection_name;
+$connection->selectDB('db_accommodation');
 
-$result=iterator_to_array($collection->find());	
+$result=iterator_to_array($connection->find([]));	
 
 include('../../api/config.php');
 for($i = 0; $i < count($result); $i++)
@@ -36,6 +39,11 @@ for($i = 0; $i < count($result); $i++)
 	{
 		$hotel_username = $result[$i]['_id'];
 		$hotel_email = $result[$i]['email'];
+		if(isset($result[$i]['country'])){
+			$hotel_country = $result[$i]['country'];
+		}else{
+			$hotel_country = 'Italy';
+		}
 	
 		// genero una password di 8 caratteri
 		$hotel_password = randomPassword();
@@ -44,7 +52,7 @@ for($i = 0; $i < count($result); $i++)
 		// memorizzo username e password su db
 		// TODO memorizzare la hash della password
 	
-		$sql = "INSERT INTO utenti(username,password,email) VALUES ('$hotel_username','$hotel_password', '$hotel_email')";
+		$sql = "INSERT INTO utenti(username,password,email,country,sent) VALUES ('$hotel_username','$hotel_password', '$hotel_email', '$hotel_country', 0)";
 		$risultati = mysqli_query($connessione, $sql);
 	}
 }	
